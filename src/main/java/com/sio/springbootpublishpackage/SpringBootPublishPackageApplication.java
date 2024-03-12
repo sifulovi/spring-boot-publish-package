e1ebb68ba60aa753c7063f2a953684ebfe4b7ddb
ove <isiful.ovi@gmail.com>
2024-03-12 14:08:23 +0600

    Init project

---
+++ src/main/java/com/sio/springbootpublishpackage/SpringBootPublishPackageApplication.java
@@ -0,0 +1,13 @@
+package com.sio.springbootpublishpackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPublishPackageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPublishPackageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sio/springbootpublishpackage/SpringBootPublishPackageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sio.springbootpublishpackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPublishPackageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
